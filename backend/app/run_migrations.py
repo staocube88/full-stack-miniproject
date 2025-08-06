f6d050aefda2970181cae4035c3219ab56c7a9e6
Oluwatobi Solomon <staocube@gmail.com>
2025-08-06 14:00:48 +0100

    Update and rename run_migrations.py to test.txt

--- backend/app/run_migrations.py
+++
@@ -1,7 +0,0 @@
-from alembic.config import Config
-from alembic import command
-import os
-
-def run_migrations():
-    alembic_cfg = Config("alembic.ini")  # path to your alembic.ini
-    command.upgrade(alembic_cfg, "head")
